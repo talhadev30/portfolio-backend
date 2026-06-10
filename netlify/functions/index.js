309772a966fdf1a296bb705449ee68b43cb227eb
talha <m.talhasalman30@gmail.com>
2026-06-11 00:10:10 +0500

    updateted

--- netlify/functions/index.js
+++
@@ -1,7 +0,0 @@
-const serverless = require("serverless-http");
-const { app } = require("../../src/app");
-
-exports.handler = async (event, context) => {
-  const handler = serverless(app);
-  return handler(event, context);
-};
--- src/app.js
+++ src/app.js
@@ -19,4 +19,4 @@ app.use("/api/auth", authroute);
 
 
 
-module.exports = { app };
+module.exports = app;

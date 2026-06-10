42cfae0a74ba778c8ee40862bc62bdca76009822
talha <m.talhasalman30@gmail.com>
2026-06-10 23:43:27 +0500

    Fix Netlify handler export

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 
 
-app.listen("3000", ()=>{
-    console.log("server is runing on port 3000");
+// app.listen("3000", ()=>{
+//     console.log("server is runing on port 3000");
     
-})
+// })
